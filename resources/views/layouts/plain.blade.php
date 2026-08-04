4f413e48cc8ae745241f3ccd80ee90312a11c5da
syunnjack <syunnda1@yahoo.co.jp>
2026-08-04 14:05:44 +0900

    Add permanent Search Console verification meta tag

--- resources/views/layouts/plain.blade.php
+++ resources/views/layouts/plain.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+  <meta name="google-site-verification" content="oDaNbbpmf2UDCH7dAoBfZ7a7aTeTsf192hUSlZ0MAdY" />
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
   <meta name="theme-color" content="#e8944a">
